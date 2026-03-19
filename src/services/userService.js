bc84a6da72552597a9b4fa251693e689c0c6e8ca
Catherine <catherineong.work@gmail.com>
2026-03-19 11:56:28 +0800

    fix: recheck dashboard hook state handling

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// 使用你提供的 Key 和正确的 ReqRes 地址
 const API_KEY = 'reqres_1fbae3e4191d463d9b79e3b1eac746d5'; 
-const API_URL = 'https://reqres.in/api/users?page=1'; // 文档要求的 GET 接口
+const API_URL = 'https://reqres.in/api/users?page=2';
 
 export const fetchUsers = async () => {
   try {
@@ -10,7 +9,7 @@ export const fetchUsers = async () => {
       headers: { 'x-api-key': API_KEY }
     });
 
-    // 转换 ReqRes 原始数据 (first_name) 为 App 所需字段 (firstName) 
+    // 转换 ReqRes 原始数据 (first_name) 为所需字段 (firstName) 
     return response.data.data.map(user => ({
       id: user.id,
       firstName: user.first_name,
